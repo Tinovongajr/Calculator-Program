1c49b08ca91c52f99bb7a5e5962410683c4a3757
Tinovongajr <65060167+Tinovongajr@users.noreply.github.com>
2024-08-31 16:06:14 +0200

    updated
    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+
 import re
 expression=""
 RPNexpression=""
@@ -33,29 +33,6 @@ def validate_expression(expression):
 
 
     return True
-
-def button_press(num):
-    global expression
-    expression += str(num)
-    equation_label.set(expression)
-
-def equals():
-
-    global expression
-    try:
-        validate_expression(expression)
-        ConvertToRPN()
-        Calculate()
-        Answer= str(stack[0])
-        equation_label.set(Answer)
-        expression=Answer
-    except ZeroDivisionError:
-        equation_label.set("Arithmetric error")
-    except SyntaxError:
-        equation_label.set("Syntax error")
-    except Exception as e:
-        equation_label.set(e)
-
 
 def ConvertToRPN():
 
@@ -140,8 +117,6 @@ def ConvertToRPN():
            for i in range(len(stack)):
               RPNlist.append(stack.pop())
 
-
-
 def Calculate():
     global RPNlist
     global stack
@@ -171,13 +146,11 @@ def Calculate():
                 stack.append(num1 ** num2)
         #print(stack)
 
-
 def reverseString(text):
     newText =""
     for i in range(len(text)):
         newText+=text[(len(text)-1)-i]
     return(newText)
-
 
 def clearx():
     global expression
@@ -188,78 +161,25 @@ def clearx():
     RPNexpression = ""
     stack = []
     RPNlist = []
-    equation_label.set("")
 
+def main():
 
-window = Tk()
-window.title("Calculator")
-window.geometry("500x500")
+    global expression
 
-equation_text = ""
-equation_label= StringVar()
+    try:
+        validate_expression(expression)
+        ConvertToRPN()
+        Calculate()
+        Answer= str(stack[0])
+        print(Answer)
+        clearx()
+    except ZeroDivisionError:
+        print("Division by zero error")
+    except SyntaxError:
+        print("Syntax error")
+    except Exception as e:
+        print(e)
 
-label = Label(window, textvariable=equation_label,font=('consolas'), bg= "white", width= 24, height = 2 )
-label.pack()
+main()
 
-frame = Frame(window)
-frame.pack()
-
-# Buttons
-button1= Button(frame, text=1, height=4, width=9, font=35, command=lambda :button_press(1))
-button1.grid(row=0, column=0)
-
-button2= Button(frame, text=2, height=4, width=9, font=35, command=lambda :button_press(2))
-button2.grid(row=0, column=1)
-
-button3= Button(frame, text=3, height=4, width=9, font=35, command=lambda :button_press(3))
-button3.grid(row=0, column=2)
-
-button4= Button(frame, text=4, height=4, width=9, font=35, command=lambda :button_press(4))
-button4.grid(row=1, column=0)
-
-button5= Button(frame, text=5, height=4, width=9, font=35, command=lambda :button_press(5))
-button5.grid(row=1, column=1)
-
-button6= Button(frame, text=6, height=4, width=9, font=35, command=lambda :button_press(6))
-button6.grid(row=1, column=2)
-
-button7= Button(frame, text=7, height=4, width=9, font=35, command=lambda :button_press(7))
-button7.grid(row=2, column=0)
-
-button8= Button(frame, text=8, height=4, width=9, font=35, command=lambda :button_press(8))
-button8.grid(row=2, column=1)
-
-button9= Button(frame, text=9, height=4, width=9, font=35, command=lambda :button_press(9))
-button9.grid(row=2, column=2)
-
-button0= Button(frame, text=0, height=4, width=9, font=35, command=lambda :button_press(0))
-button0.grid(row=3, column=0)
-
-
-
-plus= Button(frame, text='+', height=4, width=9, font=35, command=lambda :button_press('+'))
-plus.grid(row=0, column=3)
-
-minus= Button(frame, text='-', height=4, width=9, font=35, command=lambda :button_press('-'))
-minus.grid(row=1, column=3)
-
-multiply= Button(frame, text='*', height=4, width=9, font=35, command=lambda :button_press('*'))
-multiply.grid(row=2, column=3)
-
-divide= Button(frame, text='/', height=4, width=9, font=35, command=lambda :button_press('/'))
-divide.grid(row=3, column=3)
-
-
-equal= Button(frame, text='=', height=4, width=9, font=35, command=lambda :equals())
-equal.grid(row=3, column=2)
-
-decimal= Button(frame, text='.', height=4, width=9, font=35, command=lambda :button_press('.'))
-decimal.grid(row=3, column=1)
-
-clear = Button(window, text='Clear', height=4, width=12, font=35, command=lambda:clearx())
-clear.pack()
-
-
-
-window.mainloop()
 
